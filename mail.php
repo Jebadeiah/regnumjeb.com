4807b87b1a929a6778fae95cc17eb4780cb750f0
Jebadeiah <jebadeiah@gmail.com>
2025-06-22 22:52:17 -0700

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,13 +3,14 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 // Load Composer's autoloader
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 /**
  * Sends a verification email with a clickable link
  *
- * @param string $toEmail   Recipient's email
- * @param string $verifyUrl Full verification link
+ * @param string $toEmail   Recipient's email address
+ * @param string $verifyUrl Full email verification URL
+ * @return bool             True if sent, false on failure
  */
 function sendVerificationEmail($toEmail, $verifyUrl) {
     $mail = new PHPMailer(true);
@@ -19,9 +20,9 @@ function sendVerificationEmail($toEmail, $verifyUrl) {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'regnumjeb@gmail.com'; // Your Gmail
-        $mail->Password   = 'your-app-password-here'; // NOT your Gmail password
-        $mail->SMTPSecure = 'tls';
+        $mail->Username   = 'regnumjeb@gmail.com';
+        $mail->Password   = 'mfbw svor pgbb ifsc';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
 
         // Recipients
@@ -32,13 +33,19 @@ function sendVerificationEmail($toEmail, $verifyUrl) {
         $mail->isHTML(true);
         $mail->Subject = 'Please verify your email address for RegnumJeb';
         $mail->Body    = "
-          <p>Welcome to RegnumJeb!</p>
-          <p>Click the link below to verify your email and activate your account:</p>
-          <p><a href=\"$verifyUrl\">Verify Email</a></p>
+            <h2>Welcome to RegnumJeb!</h2>
+            <p>To activate your account, please verify your email by clicking the link below:</p>
+            <p><a href=\"$verifyUrl\">Verify My Email</a></p>
+            <hr>
+            <p>If you didn’t sign up, you can ignore this message.</p>
         ";
 
+        $mail->AltBody = "Welcome to RegnumJeb!\n\nPlease verify your email: $verifyUrl";
+
         $mail->send();
+        return true;
     } catch (Exception $e) {
-        error_log("Email could not be sent: {$mail->ErrorInfo}");
+        error_log("Email could not be sent to $toEmail. Error: {$mail->ErrorInfo}");
+        return false;
     }
 }
